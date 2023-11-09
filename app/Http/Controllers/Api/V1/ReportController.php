0c91e1578334339d82fa6272ae6584699daf57db
Aleksandr Lilik <lilik.aleksandr@gmail.com>
2023-11-09 19:25:31 +0200

    Monaco common race API endpoint basic tests added

--- app/Http/Controllers/Api/V1/ReportController.php
+++ app/Http/Controllers/Api/V1/ReportController.php
@@ -5,21 +5,20 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\MonacoReport;
 use Illuminate\Http\Request;
-use Spatie\ArrayToXml\ArrayToXml;
+//use Spatie\ArrayToXml\ArrayToXml;
 use Mtownsend\ResponseXml\Providers\ResponseXmlServiceProvider;
 
 class ReportController extends Controller
 {
     public function index(Request $request): string|null
     {
-        $format= $request->only(['format']);
-
         $monacoRace = new MonacoReport();
         $raceReport = $monacoRace->buildRaceReport('pilot_name');
 
-        $response = null;
-        if ($format['format'] === 'json' || !isset($format['format'])) {
-            return response()->json($raceReport);
+        $format = $request->filled(['format']) ?  $request->only(['format']) : null;
+
+        if (!$format || $format['format'] === 'json') {
+            return json_encode(response()->json($raceReport, 200, [],0));
         }
         if ($format['format'] === 'xml') {
 //            $xml = ArrayToXml::convert($raceReport);
